4fa6e20da16592aae86c1b70fac268a4119f2fe8
EthanDriscoll1 <153683554+EthanDriscoll1@users.noreply.github.com>
2024-07-24 15:08:10 -0700

    Fixed errors

--- Assets/Scripts/Controllers/End screen.cs
+++ Assets/Scripts/Controllers/End screen.cs
@@ -4,16 +4,13 @@ using UnityEngine;
 
 public class EndScreen : MonoBehaviour
 {
-    public Text endMessageText;
 
     public void Mainmenu()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+
     }
 
     public void QuitGame()
-    {
-         public void QuitGame()
     {
         Application.Quit();
     }
