edaec7e770c23d73c296b5f21317d8d9c1349f18
EthanDriscoll1 <153683554+EthanDriscoll1@users.noreply.github.com>
2024-07-26 14:40:12 -0700

    Fixing Audio, Prefabs base set

--- Assets/Scripts/Controllers/AudioManager.cs
+++ Assets/Scripts/Controllers/AudioManager.cs
@@ -1,6 +1,7 @@
 using System;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
 
 public class AudioManager : MonoBehaviour
 {
@@ -22,8 +23,26 @@ public class AudioManager : MonoBehaviour
             s.Source.clip = s.Clip;
             s.Source.volume = s.Volume;
             s.Source.pitch = s.Pitch;
+            s.Source.loop = s.Loop;
         }
     }
+
+    private void Update()
+    {
+        int Index = SceneManager.GetActiveScene().buildIndex;
+        if (Index == 4)
+        {
+            Stop("MainBG");
+            Play("BossBG");
+        }
+        else
+        {
+            Stop("BossBG");
+            Play("MainBG");
+        }
+
+    }
+
     public void Play(string Name)
     {
         Sound s = Array.Find(Sounds, Sound => Sound.Name == Name);
@@ -34,6 +53,19 @@ public class AudioManager : MonoBehaviour
         Debug.Log(Name);
         s.Pitch = UnityEngine.Random.Range(s.Pitch - 0.2f, s.Pitch + 0.2f);
         s.Source.Play();
+    }
+
+    public void Stop(string Name)
+    {
+        Sound s = Array.Find(Sounds, Sound => Sound.Name == Name);
+        if (s == null)
+        {
+            Debug.Log("Null");
+        }
+        Debug.Log(Name);
+        s.Pitch = UnityEngine.Random.Range(s.Pitch - 0.2f, s.Pitch + 0.2f);
+        s.Source.Stop();
+
     }
 
     //FindObjectOfType<AudioManager>().Play("SOUNDNAME");
--- Assets/Scripts/Controllers/Sound.cs
+++ Assets/Scripts/Controllers/Sound.cs
@@ -11,4 +11,5 @@ public class Sound
     [Range(0f, 1f)] public float Volume = 1;
     [Range(0.1f, 3f)] public float Pitch = 1;
     public AudioSource Source;
+    public bool Loop;
 }
